aa8f6dda8d936e1b31e0fb63d5b71a3853742079
Matt Stratton <matt.stratton@gmail.com>
2013-01-20 11:41:16 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JustplainnutzApp::Application.config.secret_token = '7e3b079626f49bf3ba90da67540eb828d46887583b19693c129a4c0ea5efd6aab26c824d60a8d41d7f5f8559c257f94e67caac478a19c4f1e974c92324d37e2c'
